49bbf3d10c9c796be75b0aa4504d4d36c06fba03
petrutcoman <petrut.coman@gmail.com>
2021-06-17 22:29:31 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '../src/index.css';
+
+export default class Sample extends React.Component {
+    render(){
+    return (
+        <label htmlFor="#">
+            <input type="checkbox" id="#"/>
+            <div className="card">
+                <div className="front">Front</div>
+                <div className="back">Back</div>
+            </div>
+        </label>
+    )
+ 
+    }
+}
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Sample />,
+    document.getElementById('root')
+  );
+  
