d879f84eb2895a3c557fb3a9496c6263482f8045
petrutcoman <petrut.coman@gmail.com>
2021-06-18 14:59:39 +0300

    small logic updates

--- src/index.js
+++ src/index.js
@@ -5,28 +5,70 @@ import '../src/index.css';
 export default class Sample extends React.Component {
     constructor(props){
         super(props);
-        this.state={tlsarray:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]};
-        
+        this.state={valuesarray:[],clckcount:0,tlsarray:[],first:'',second:'',};
+        let str=['A','B','C','D','E','F','G','H'];
+        this.state.valuesarray=str.concat(str);
+        this.shuffleArray(this.state.valuesarray);
+        this.initArray(this.state.tlsarray);
       }
-    initArray(){
-        let tmparr=[];
+      initArray(arr){
         for (let i=0;i<16;i++){
-            tmparr.push(i);
+            arr.push(false);
         }
-        this.setState({tlsarray:tmparr});
-    }
+        return(arr);  
+      }
+      shuffleArray(arr){
+        for (let i = arr.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+       }
+       mesage(ind,val){
+        let nr=this.state.clckcount; 
+        let tmparr=this.state.tlsarray;
+        if (this.state.clckcount===0){
+            this.setState({first:val})
+        }else{
+            if(this.state.clckcount===1){
+                if(this.state.first===val){
+                    console.log("pair ( "+val+","+val+")");
+                }
+                this.setState({second:val})
+               
+        }else{ this.setState({first:'',second:''})}
+        }
+        if (nr>=2){
+            nr=0;
+            tmparr.fill(false);
+            this.setState({clckcount:nr,tlsarray:tmparr});
+        }else{
+            nr=nr+1;
+            tmparr[ind]=true;
+            this.setState({clckcount:nr,tlsarray:tmparr});
+        }
+        
+
+       }
+       handleChange(ev,ind){
+        if(this.state.tlsarray[ind] !== ev.target.checked) {
+            let tmparr=this.state.tlsarray;
+            tmparr[ind]=ev.target.checked;
+            this.setState({tlsarray:tmparr});
+         }
+       }
     render(){
         
     return (
         <div className="board">
             <div className="overlay">
-                {this.state.tlsarray.map(
-                (tlsarray,index) => (
-                    <label key={index.toString()} htmlFor={index.toString()}>
-                    <input type="checkbox" id={index.toString()}/>
+                {this.state.valuesarray.map(
+                (valuesarray,index) => ( valuesarray!==null &&
+                    <label key={index.toString()}  htmlFor={index.toString()}>
+                    <input type="checkbox" id={index.toString()} checked={this.state.tlsarray[index]} onChange={(event,index)=>this.handleChange(event,index)} onClick={()=>this.mesage(index,valuesarray)}/>
                     <div className="card">
                         <div className="front">Front</div>
-                        <div className="back">{index.toString()}</div>
+                        <div className="back">{valuesarray}</div>
                     </div>
                 </label>))}
             </div>
