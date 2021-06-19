43e15e59705a4cfbdd399ed1cb484318aaedde16
petrutcoman <petrut.coman@gmail.com>
2021-06-19 12:53:45 +0300

    logic/css updates

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import '../src/index.css';
 export default class Sample extends React.Component {
     constructor(props){
         super(props);
-        this.state={valuesarray:[],clckcount:0,tlsarray:[],first:'',second:'',};
+        this.state={clckcount:0,first:{},objarr:[]};
+        this.initObjArr(this.state.objarr);
+      }
+      initObjArr(arr){
         let str=['A','B','C','D','E','F','G','H'];
-        this.state.valuesarray=str.concat(str);
-        this.shuffleArray(this.state.valuesarray);
-        this.initArray(this.state.tlsarray);
+        str=str.concat(str);
+        this.shuffleArray(str);
+        for (let nr=0;nr<16;nr++){
+            arr.push({id:nr,val:str[nr],isflipped:false,isenabled:true});
+        }
+        return(arr);
       }
       initArray(arr){
         for (let i=0;i<16;i++){
@@ -24,51 +30,62 @@ export default class Sample extends React.Component {
         }
         return arr;
        }
-       mesage(ind,val){
-        let nr=this.state.clckcount; 
-        let tmparr=this.state.tlsarray;
-        if (this.state.clckcount===0){
-            this.setState({first:val})
-        }else{
-            if(this.state.clckcount===1){
-                if(this.state.first===val){
-                    console.log("pair ( "+val+","+val+")");
-                }
-                this.setState({second:val})
-               
-        }else{ this.setState({first:'',second:''})}
-        }
-        if (nr>=2){
-            nr=0;
-            tmparr.fill(false);
-            this.setState({clckcount:nr,tlsarray:tmparr});
-        }else{
-            nr=nr+1;
-            tmparr[ind]=true;
-            this.setState({clckcount:nr,tlsarray:tmparr});
-        }
-        
-
+       resetArray(arr){
+           for (let i=0;i<16;i++){
+               if (arr[i]['isenabled']===true){
+               arr[i]['isflipped']=false;}
+           }
+           return arr;
        }
-       handleChange(ev,ind){
-        if(this.state.tlsarray[ind] !== ev.target.checked) {
-            let tmparr=this.state.tlsarray;
-            tmparr[ind]=ev.target.checked;
-            this.setState({tlsarray:tmparr});
-         }
+       mesage(ev,obj){
+        if (obj['isenabled']===true){
+            obj['isflipped'] = ev.target.checked;
+            let tmparr=this.state.objarr;
+            tmparr[obj['id']]=obj;
+            this.setState({objarr:tmparr});
+        }
+        let nr=this.state.clckcount; 
+        switch(nr){
+            case 0:
+                if (obj['isenabled']===true){
+                nr=nr+1;
+                this.setState({first:obj,clckcount:nr});
+                }
+            break;
+            default:
+                if (obj['isenabled']===true){
+                if (obj!==this.state.first){
+                if (this.state.first['val']===obj['val']){
+                    let tmparr=this.state.objarr;
+                    tmparr[tmparr.indexOf(obj)]['isenabled']=false;
+                    tmparr[tmparr.indexOf(this.state.first)]['isenabled']=false;
+                    this.setState({objarr:tmparr});
+                }
+                nr=0;
+                let arr=this.state.objarr;
+                setTimeout(()=>{this.resetArray(arr)},1000);
+                setTimeout(()=>{this.setState({objarr:arr,clckcount:nr})},1000);
+                
+            }     
+            }
+            }   
+              
+        }
+       handleChange(ev,obj){
+            //used just for state of obj for now flipped/not flipped
        }
     render(){
         
     return (
         <div className="board">
             <div className="overlay">
-                {this.state.valuesarray.map(
-                (valuesarray,index) => ( valuesarray!==null &&
+                {this.state.objarr.map(
+                (obj,index) => ( obj!==null &&
                     <label key={index.toString()}  htmlFor={index.toString()}>
-                    <input type="checkbox" id={index.toString()} checked={this.state.tlsarray[index]} onChange={(event,index)=>this.handleChange(event,index)} onClick={()=>this.mesage(index,valuesarray)}/>
+                    <input type="checkbox" id={index.toString()} checked={obj.isflipped} onChange={(event)=>this.handleChange(event,obj)} onClick={(event)=>this.mesage(event,obj)}/>
                     <div className="card">
-                        <div className="front">Front</div>
-                        <div className="back">{valuesarray}</div>
+                        <div className="front"></div>
+                        <div className="back">{obj.val}</div>
                     </div>
                 </label>))}
             </div>
