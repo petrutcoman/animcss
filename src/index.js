09967aa1dda338903ebe1114e8ab165a1abcf116
petrutcoman <petrut.coman@gmail.com>
2021-06-22 14:33:01 +0300

    reset and timer prep

--- src/index.js
+++ src/index.js
@@ -5,8 +5,42 @@ import '../src/index.css';
 export default class Sample extends React.Component {
     constructor(props){
         super(props);
-        this.state={clckcount:0,first:{},objarr:[]};
+        this.state={clckcount:0,first:{},objarr:[],moves:0,time: 0,isOn: false,start: 0};
         this.initObjArr(this.state.objarr);
+        this.startTimer = this.startTimer.bind(this)
+        this.stopTimer = this.stopTimer.bind(this)
+        this.resetTimer = this.resetTimer.bind(this)
+      }
+
+      startTimer() {
+        this.setState({
+          isOn: true,
+          time: this.state.time,
+          start: Date.now() - this.state.time
+        })
+        this.timer = setInterval(() => this.setState({
+          time: Date.now() - this.state.start
+        }), 1);
+      }  
+      stopTimer() {
+        this.setState({isOn: false})
+        clearInterval(this.timer)
+      }  
+      resetTimer() {
+        this.setState({time: 0, isOn: false})
+      }
+
+      resetCards(){
+        let arr=this.state.objarr
+        for (let i=0;i<16;i++){
+            arr[i]['isenabled']=true;
+            arr[i]['isflipped']=false;
+        }
+          arr=[];
+          setTimeout(()=>{this.initObjArr(arr)},700);
+          setTimeout(()=>{this.setState({objarr:arr, moves:0})},700);
+
+
       }
       initObjArr(arr){
         let str=['/Assets/1.png','/Assets/2.png','/Assets/3.png','/Assets/4.png','/Assets/5.png','/Assets/6.png','/Assets/7.png','/Assets/8.png'];
@@ -16,12 +50,6 @@ export default class Sample extends React.Component {
             arr.push({id:nr,val:str[nr],isflipped:false,isenabled:true});
         }
         return(arr);
-      }
-      initArray(arr){
-        for (let i=0;i<16;i++){
-            arr.push(false);
-        }
-        return(arr);  
       }
       shuffleArray(arr){
         for (let i = arr.length - 1; i > 0; i--) {
@@ -63,22 +91,25 @@ export default class Sample extends React.Component {
                 }
                 nr=0;
                 let arr=this.state.objarr;
-                setTimeout(()=>{this.resetArray(arr)},1000);
-                setTimeout(()=>{this.setState({objarr:arr,clckcount:nr})},1000);
+                setTimeout(()=>{this.resetArray(arr)},700);
+                setTimeout(()=>{this.setState({objarr:arr,clckcount:nr})},700);
                 
-            }     
+            }
+            this.setState({moves:this.state.moves+1});     
             }
             }   
               
         }
+        
        handleChange(ev,obj){
             //used just for state of obj for now flipped/not flipped
        }
     render(){
         
     return (
-        <div className="board">
-            <div className="overlay">
+        <div className="container">
+            <div className="board">
+              <div className="overlay">
                 {this.state.objarr.map(
                 (obj,index) => ( obj!==null &&
                     <label key={index.toString()}  htmlFor={index.toString()}>
@@ -88,9 +119,14 @@ export default class Sample extends React.Component {
                         <div className="back" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + obj['val']})` }}></div>
                     </div>
                 </label>))}
+                </div>
+            </div>
+            <div className="textstuff">
+                <div>Moves: {this.state.moves}</div>
+                <div>Time: </div>
+                <button onClick={()=>this.resetCards()}>Reset</button>
             </div>
         </div>
-
     )
  
     }
